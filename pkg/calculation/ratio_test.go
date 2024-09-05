1b16f6ae55d4d27c11ed498e7076a0488263605c
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2024-09-05 22:17:05 +0200

    rename ratio tests functions

--- pkg/calculation/ratio_test.go
+++ pkg/calculation/ratio_test.go
@@ -48,8 +48,8 @@ func (suite *ratioTestSuite) SetupTest() {
 	}
 }
 
-// TestContext_GetAssetRatio runs the smoke test.
-func (suite *ratioTestSuite) TestContext_GetAssetRatio() {
+// TestGetAssetRatio runs the smoke test.
+func (suite *ratioTestSuite) TestGetAssetRatio() {
 	m, err := suite.ctx.GetAssetRatio(suite.ctx.AssetContext.Assets)
 	keys := suite.ctx.AssetContext.GetAssetKeyMap()
 
@@ -58,8 +58,8 @@ func (suite *ratioTestSuite) TestContext_GetAssetRatio() {
 	assert.Equal(suite.T(), big.NewRat(70248572748941, 1855638572748941), m[keys["B"]], "asset ratio should match")
 }
 
-// TestContext_GetAssetRatio runs the smoke test and makes sure that the sum of asset ratios add up to 1.
-func (suite *ratioTestSuite) TestContext_GetAssetRatio_One_Sum() {
+// TestGetAssetRatio_One_Sum runs the smoke test and makes sure that the sum of asset ratios add up to 1.
+func (suite *ratioTestSuite) TestGetAssetRatio_One_Sum() {
 	m, err := suite.ctx.GetAssetRatio(suite.ctx.AssetContext.Assets)
 
 	assert.NoError(suite.T(), err, "should not return error")
@@ -72,8 +72,8 @@ func (suite *ratioTestSuite) TestContext_GetAssetRatio_One_Sum() {
 	assert.Equal(suite.T(), big.NewRat(1, 1), sum, "sum should be 1")
 }
 
-// TestContext_GetAssetRatio_No_Transactions runs tests without any transactions.
-func (suite *ratioTestSuite) TestContext_GetAssetRatio_No_Transactions() {
+// TestGetAssetRatio_No_Transactions runs tests without any transactions.
+func (suite *ratioTestSuite) TestGetAssetRatio_No_Transactions() {
 	suite.ctx.TransactionContext.Transactions = []*transaction.Tx{}
 	for _, a := range suite.ctx.TransactionContext.Assets {
 		a.Transactions = []*transaction.Tx{}
@@ -84,14 +84,14 @@ func (suite *ratioTestSuite) TestContext_GetAssetRatio_No_Transactions() {
 	assert.EqualError(suite.T(), err, "sum of asset worth is zero", "should throw an error if assets have no worth")
 }
 
-// TestContext_GetAssetRatio_No_Assets runs the test with no asset argument.
-func (suite *ratioTestSuite) TestContext_GetAssetRatio_No_Assets() {
+// TestGetAssetRatio_No_Assets runs the test with no asset argument.
+func (suite *ratioTestSuite) TestGetAssetRatio_No_Assets() {
 	_, err := suite.ctx.GetAssetRatio([]*asset.Asset{})
 	assert.EqualError(suite.T(), err, "sum of asset worth is zero", "should throw an error if assets have no worth")
 }
 
-// TestContext_GetAssetRatio_Zero_Portfolio_Worth runs the test with a complete loss to make sure the correct error is returned
-func (suite *ratioTestSuite) TestContext_GetAssetRatio_Zero_Portfolio_Worth() {
+// TestGetAssetRatio_Zero_Portfolio_Worth runs the test with a complete loss to make sure the correct error is returned
+func (suite *ratioTestSuite) TestGetAssetRatio_Zero_Portfolio_Worth() {
 	for _, a := range suite.ctx.AssetContext.Assets {
 		a.UnitPrice = big.NewRat(0, 1)
 	}
@@ -101,8 +101,8 @@ func (suite *ratioTestSuite) TestContext_GetAssetRatio_Zero_Portfolio_Worth() {
 	assert.EqualError(suite.T(), err, "sum of asset worth is zero", "should throw an error if assets have no worth")
 }
 
-// TestContext_GetAssetRatio_No_Context runs the test without transaction and asset contexts.
-func (suite *ratioTestSuite) TestContext_GetAssetRatio_No_Context() {
+// TestGetAssetRatio_No_Context runs the test without transaction and asset contexts.
+func (suite *ratioTestSuite) TestGetAssetRatio_No_Context() {
 	suite.ctx.AssetContext = nil
 	suite.ctx.TransactionContext = nil
 
